da6691c57a4e94289cc434b2808ebbed2f0ad058
yiwentang <yushantang05@gmail.com>
2026-03-06 21:17:50 +0800

    fixed:fix CORS error

--- api/agent/chat.js
+++ api/agent/chat.js
@@ -199,9 +199,17 @@ async function callDeepSeek(messages, includeTools = true) {
   return json?.choices?.[0]?.message
 }
 
+function setCorsHeaders(res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+}
+
 export default async function handler(req, res) {
+  setCorsHeaders(res)
+
   if (req.method === 'OPTIONS') {
-    return res.status(200).end()
+    return res.status(204).end()
   }
 
   if (req.method !== 'POST') {
